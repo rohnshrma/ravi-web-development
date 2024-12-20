f36d49dd5f1d4ede82d2d33371dbb63ada61fac6
rohnshrma <rohn11shrma@gmail.com>
2024-12-20 12:34:20 +0530

    loops class 2

--- js/loops.js
+++ js/loops.js
@@ -45,3 +45,91 @@
 
 // marks.push(34);
 // marks.pop();
+
+// initialization | condition | update
+
+// var i = 0
+// while (condition){
+// code to be executed
+// i++
+// i+=1
+// }
+
+// var marks = [12, 34, 4, 67, 89, 67];
+
+// var max = marks[0];
+
+// var i = 0;
+// while (i < marks.length) {
+//   var currentMark = marks[i];
+
+//   if (currentMark > max) {
+//     max = currentMark;
+//   }
+
+//   i++;
+// }
+
+// console.log(max);
+
+// var marks = [12, 34, 4, 67, 89, 67];
+
+// var max = marks[0],
+//   i = 0;
+
+// while (i < marks.length) max = Math.max(max, marks[i++]);
+// console.log(max);
+
+// var email = prompt("Enter your email : ").toLowerCase();
+// while (email.length < 8 || !email.includes("@") || !email.endsWith(".com"))
+//   email = prompt("Enter your email : ").toLowerCase();
+
+// console.log(email);
+
+// ============= approach 1
+
+// var isValid = false;
+
+// while (!isValid) {
+//   var password = prompt(
+//     "Enter a password (must contain 1 number, 1 capital letter and 1 special character ) : "
+//   );
+//   var hasNumber = false;
+//   var hasUpperCase = false;
+//   var hasSpecialSymbol = false;
+
+//   for (var i = 0; i < password.length; i++) {
+//     var char = password[i];
+//     if (char >= "0" && char <= "9") hasNumber = true;
+//     else if (char >= "A" && char <= "Z") hasUpperCase = true;
+//     else if (char.match(/[^a-zA-Z0-9]/)) hasSpecialSymbol = true;
+//     if (hasNumber && hasUpperCase && hasSpecialSymbol) {
+//       isValid = true;
+//       break;
+//     }
+//   }
+
+//   if (!isValid) {
+//     alert(`Invalid password.Please try again`);
+//   } else {
+//     alert("Password accepted");
+//   }
+// }
+
+// var isValid = false;
+// while (!isValid) {
+//   var password = prompt(
+//     "Enter a password (must contain 1 number, 1 capital letter and 1 special character ) : "
+//   );
+
+//   if (
+//     /[A-Z]/.test(password) &&
+//     /\d/.test(password) &&
+//     /[!@#$%^&*()_+{}|<>?~`]/.test(password)
+//   ) {
+//     isValid = true;
+//     alert("Valid Password");
+//   } else {
+//     alert("Invalid Password");
+//   }
+// }
