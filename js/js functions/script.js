1f3f0be9582f36aafcd18c9c7fbaf2ffdd18d213
rohnshrma <rohn11shrma@gmail.com>
2024-12-24 11:29:29 +0530

    Refactor addition function to return result and clean up commented code

--- js/js functions/script.js
+++ js/js functions/script.js
@@ -1,44 +1,19 @@
-// javascript functions
+// function and return keyword
+function addition(a, b) {
+  var result = a + b;
+  return result;
+}
 
-// function functionName(parameters(variables)){
-// code to be executed
+var res = addition(12, 23);
+
+console.log(res);
+
+// function printing() {
+//   console.log(1);
+//   console.log(2);
+//   return;
+//   console.log(3);
+//   console.log(4);
 // }
 
-function addition(a, b) {
-  console.log(a + b);
-}
-
-// addition(12, 23);
-// addition(124, 233);
-// addition(125, 273);
-// addition(152, 238);
-
-// named funciton  | function declaration
-function greeting(name) {
-  console.log("welcome", name);
-}
-
-// greeting("ravi");
-
-// function expressions : anonymous function && arrow function
-
-// anonymous function
-var multiply = function (a, b) {
-  console.log(a * b);
-};
-// multiply(12, 23);
-
-// arrow function
-// var divide = (a, b) => {
-//   console.log(a / b);
-// };
-
-// divide(123, 2);
-
-// when exactly a single parameter is passed : ommit "()"
-var calcSquare = (num) => {
-  console.log(num ** 2);
-};
-
-// all your function does is return something  : "{}" and "return" can be ommited
-var calcCube = (num) => num ** 3;
+// printing();
