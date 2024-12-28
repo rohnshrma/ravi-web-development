47069434bd8da1e31e3b3fb5c58578b959ce3f76
rohnshrma <rohn11shrma@gmail.com>
2024-12-28 11:56:31 +0530

    Implement Tic Tac Toe game with HTML, CSS, and JavaScript; add game logic and UI elements

---
+++ js/js functions/script.js
@@ -0,0 +1,68 @@
+// tic tac toe
+
+var board = ["#", " ", " ", " ", " ", " ", " ", " ", " ", " "];
+var currentPlayer = "X";
+var isGameActive = true;
+
+const turnInfo = document.getElementById("turn-info");
+const gameResult = document.getElementById("game-result");
+const cells = document.querySelectorAll(".cell");
+const resetBtn = document.getElementById("reset-btn");
+
+function displayBoard() {
+  cells.forEach((cell, i) => {
+    if (i >= 0) {
+      cell.textContent = board[i + 1];
+    }
+  });
+}
+
+function winCheck(board, marker) {
+  return (
+    (board[1] === marker && board[2] === marker && board[3] === marker) ||
+    (board[4] === marker && board[5] === marker && board[6] === marker) ||
+    (board[7] === marker && board[8] === marker && board[9] === marker) ||
+    (board[1] === marker && board[4] === marker && board[7] === marker) ||
+    (board[2] === marker && board[6] === marker && board[8] === marker) ||
+    (board[3] === marker && board[6] === marker && board[9] === marker) ||
+    (board[1] === marker && board[5] === marker && board[9] === marker) ||
+    (board[3] === marker && board[5] === marker && board[7] === marker)
+  );
+}
+
+function drawCheck(board) {
+  return board.every((cell, index) => {
+    index === 0 || cell !== " ";
+  });
+}
+
+function handleCellClick(e) {
+  const index = e.target.getAttribute("data-index");
+  console.log(index);
+  if (board[index] !== " " || !isGameActive) return;
+  board[index] = currentPlayer;
+  displayBoard();
+
+  if (winCheck(board, currentPlayer)) {
+    gameResult.textContent = `${currentPlayer} Wins!`;
+  } else if (drawCheck(board)) {
+    gameResult.textContent = "Game Draw!";
+  } else {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    turnInfo.textContent = `${currentPlayer}'s turn`;
+  }
+}
+
+function resetGame() {
+  board = ["#", " ", " ", " ", " ", " ", " ", " ", " ", " "];
+  currentPlayer = "X";
+  isGameActive = true;
+  gameResult.textContent = "";
+  turnInfo.textContent = "Player X's Turn";
+  displayBoard();
+}
+
+cells.forEach((cell) => cell.addEventListener("click", handleCellClick));
+resetBtn.addEventListener("click", resetGame);
+
+displayBoard();
